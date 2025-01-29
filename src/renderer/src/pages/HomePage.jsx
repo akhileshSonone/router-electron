c096c28368c863bf3212dd4d3035ef81ada56811
akhileshSonone <a.sonone@smartshiphub.com>
2025-01-29 10:41:01 +0530

    remove useEffect

--- src/renderer/src/pages/HomePage.jsx
+++ src/renderer/src/pages/HomePage.jsx
@@ -1,33 +1,32 @@
-import { useEffect, useState } from 'react'
 import { Link } from 'react-router'
 
-const UpdateDialog = (updateStatus) => {
-  if (!updateStatus) return null
-
-  return (
-    <div className="update-status">
-      {updateStatus.type === 'progress' ? (
-        <div className="progress-bar">{updateStatus.message}</div>
-      ) : (
-        <div className={`message ${updateStatus.type}`}>{updateStatus.message}</div>
-      )}
-    </div>
-  )
-}
+//const UpdateDialog = (updateStatus) => {
+//  if (!updateStatus) return null
+//
+//  return (
+//    <div className="update-status">
+//      {updateStatus.type === 'progress' ? (
+//        <div className="progress-bar">{updateStatus.message}</div>
+//      ) : (
+//        <div className={`message ${updateStatus.type}`}>{updateStatus.message}</div>
+//      )}
+//    </div>
+//  )
+//}
 
 const HomePage = () => {
-  const [updateStatus, setUpdateStatus] = useState(null)
-
-  useEffect(() => {
-    // Listen for update status changes
-    const removeListener = async () => {
-      const receivedStatus = await window.api.onUpdateStatus()
-      setUpdateStatus(receivedStatus)
-    }
-
-    removeListener()
-    return () => removeListener()
-  }, [])
+  //const [updateStatus, setUpdateStatus] = useState(null)
+  //
+  //useEffect(() => {
+  //  // Listen for update status changes
+  //  const removeListener = async () => {
+  //    const receivedStatus = await window.api.onUpdateStatus()
+  //    setUpdateStatus(receivedStatus)
+  //  }
+  //
+  //  removeListener()
+  //  return () => removeListener()
+  //}, [])
 
   return (
     <div>
